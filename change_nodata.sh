f8fcc244bff7ddbeea5728b6b5e62fed7ec54f96
Sudipta Basak <basaks@gmail.com>
2018-07-18 08:53:36 +1000

    Create change_nodata.sh

---
+++ change_nodata.sh
@@ -0,0 +1,5 @@
+# set values less than -9999 to -9999
+# follow up by assigning nodata to -9999
+gdal_calc.py -A Potassium_2016.tif --outfile=Potassium_2016_temp.tif --calc="-9999*(A<-9999)+A*(A>=-9999)"
+gdalwarp -dstnodata -9999 Potassium_2016_tmp.tif Potassium_2016_NoData.tif
+rm Potassium_2016_temp.tif
