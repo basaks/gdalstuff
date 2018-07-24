148185455391207a388de0aab4d128255a9eb07c
Sudipta Basak <basaks@gmail.com>
2018-07-25 06:11:19 +1000

    Update change_nodata.sh

--- change_nodata.sh
+++ change_nodata.sh
@@ -1,8 +1,8 @@
 # set values less than -9999 to -9999
 # follow up by assigning nodata to -9999
 
-gdal_calc.py -A Potassium_2016.tif --outfile=Potassium_2016_temp.tif --calc="-9999*(A<-9999)+A*(A>=-9999)"
+gdal_calc.py -A Rad_ratio_K_Th.tif --overwrite --NoDataValue=-9999 --outfile=Rad_ratio_K_Th_temp.tif --calc="-9999*(A<-9999)+A*(A>=-9999)"
 
-# stragely sometimes the last two steps are not required
-gdalwarp -dstnodata -9999 Potassium_2016_tmp.tif Potassium_2016_NoData.tif
-rm Potassium_2016_temp.tif
+# 
+# gdalwarp -dstnodata -9999 Potassium_2016_tmp.tif Potassium_2016_NoData.tif
+# rm Potassium_2016_temp.tif
