7663eb2265f271e3fda277c2120d8c44616c732c
Sudipta Basak <basaks@gmail.com>
2018-09-22 18:49:10 +1000

    Update to_tiled_tifs.sh

--- to_tiled_tifs.sh
+++ to_tiled_tifs.sh
@@ -1,1 +1,1 @@
-gdal_translate -of GTiff -co "BLOCKXSIZE=128" -co "BLOCKYSIZE=128" -co "TILED=YES" dem_fill.tif dem_fill_tiled.tif
+gdal_translate -of GTiff -co "BLOCKXSIZE=128" -co "BLOCKYSIZE=128" -co "TILED=YES" -co compress=LZW dem_fill.tif dem_fill_tiled.tif
