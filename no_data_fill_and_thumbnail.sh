126d8099934a6e5e159d71b158300ea9fb6f85d7
Sudipta Basak <basaks@gmail.com>
2018-09-23 08:22:54 +1000

    Update no_data_fill_and_thumbnail.sh

--- no_data_fill_and_thumbnail.sh
+++ no_data_fill_and_thumbnail.sh
@@ -1,36 +1,46 @@
 #PBS -P ge3
 #PBS -q normalbw
-#PBS -l walltime=10:00:00,mem=256GB,ncpus=28,jobfs=100GB
+#PBS -l walltime=2:00:00,mem=256GB,ncpus=28,jobfs=100GB
 #PBS -l wd
 #PBS -j oe
 
 module unload intel-cc
 module unload intel-cc
 module load python3/3.5.2 python3/3.5.2-matplotlib
-module gdal/2.0.0 geos/3.5.0 gcc/4.9.0
-module load parallel
+module load hdf5/1.8.10 gdal/2.0.0 geos/3.5.0 proj/4.9.3 gcc/4.9.0
+module load openmpi/1.8 parallel
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.5/site-packages:$PYTHONPATH
+export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.5.2/bin/python3
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+source $HOME/.local/bin/virtualenvwrapper.sh
+export WORKON_HOME=/g/data/ge3/john/venvs/
+export UNCOVER=$HOME/uncover-ml
+workon class
 
-export COMMON_OPTIONS="-co BLOCKXSIZE=128 -co BLOCKYSIZE=128 -co TILED=YES -co COMPRESS=LZW"
-covar_file=/g/data/ge3/covariates/national_albers/albers_tifs.txt
-outdir=/g/data/ge3/covariates/national_albers/albers_tifs_filled
+export COMMON_OPTIONS="-co BLOCKXSIZE=128 -co BLOCKYSIZE=128 -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=NO"
 
-# number of pixels to interpolate
+covar_file=/g/data/ge3/covariates/national_albers/covar.txt
+
+outdir=/g/data/ge3/covariates/national_albers/albers_tifs
+
+# number of pixels to interpolate to
 extent=3
-
-# while IFS='' read -r line || [[ -n "$line" ]]; do
-#      echo converting $line and output ${outdir}/filled_${line##*/} ${outdir}/filled_th_${line##*/};
-# done < /g/data/ge3/covariates/national_albers/albers_tifs.txt
 
 function fill {
     outdir=$1
     f=$3
     extent=$2
-    echo fill nodata in $f and output ${outdir}/filled_${f##*/} and thumbnail ${outdir}/filled_th_${f##*/} with extent: $extent;
-    python gdal_fillnodata.py $COMMON_OPTIONS -md $extent $f ${outdir}/filled_${f##*/};
-    echo now creating thumbnail ${outdir}/filled_th_${f##*/};
-    gdalwarp -overwrite $COMMON_OPTIONS -tr 850 850 ${outdir}/filled_${f##*/} ${outdir}/filled_th_${f##*/};
+    echo =====================================================================;
+    echo projecting $f into ${outdir}/${f##*/};
+    gdalwarp -overwrite -t_srs EPSG:3577 $COMMON_OPTIONS $f ${outdir}/${f##*/};
+    echo fill nodata in ${outdir}/${f##*/} and write to ${outdir}/filled_${f##*/} with extent $extent;
+    python gdal_fillnodata.py $COMMON_OPTIONS -md $extent ${outdir}/${f##*/} ${outdir}/filled_${f##*/};
+    echo thumbnail ${outdir}/filled_${f##*/} ${outdir}/filled_th_${f##*/};
+    gdalwarp -overwrite $COMMON_OPTIONS -tr 900 900 ${outdir}/filled_${f##*/} ${outdir}/filled_th_${f##*/};
 }
 
 export -f fill
 
-cat ${covar_file} | parallel -j 6 fill ${outdir} $extent
+cat ${covar_file} | parallel -j 28 fill ${outdir} $extent
