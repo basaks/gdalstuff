cd1f060601b7e3948abf999e648593eed9da0444
Sudipta Basak <basaks@gmail.com>
2018-10-14 18:48:40 +1100

    Create mpi_for_loop.sh

---
+++ mpi_for_loop.sh
@@ -0,0 +1,65 @@
+#PBS -P ge3
+#PBS -q normalbw
+#PBS -l walltime=2:00:00,mem=256GB,ncpus=28,jobfs=100GB
+#PBS -l wd
+#PBS -j oe
+
+module unload intel-cc
+module unload intel-cc
+module load python3/3.5.2 python3/3.5.2-matplotlib
+module load hdf5/1.8.10 gdal/2.0.0 geos/3.5.0 proj/4.9.3 gcc/4.9.0
+module load openmpi/1.8 parallel
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.5/site-packages:$PYTHONPATH
+export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.5.2/bin/python3
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+source $HOME/.local/bin/virtualenvwrapper.sh
+export WORKON_HOME=/g/data/ge3/john/venvs/
+export UNCOVER=$HOME/uncover-ml
+workon class
+
+export COMMON_OPTIONS="-co BLOCKXSIZE=256 -co BLOCKYSIZE=256 -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=YES"
+# export GDAL_OPTS="-multi -co NUM_THREADS=ALL_CPUS -wo NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 50000"
+covar_file=/g/data/ge3/covariates/national_filled/covar.txt
+
+outdir=/g/data/ge3/covariates/national_filled
+
+# number of pixels to interpolate to
+export extent=5
+export rows=10
+export cols=200
+export partitions=20
+
+# new no data
+export NODATA=-9999
+
+function project {
+    outdir=$1
+    f=$2
+    echo =====================================================================;
+    echo projecting ${f} into ${outdir}/albers_${f##*/};
+    gdalwarp -overwrite -r med -t_srs EPSG:3577 $COMMON_OPTIONS ${f} ${outdir}/albers_${f##*/};
+}
+
+
+function fill {
+    outdir=$1
+    f=$2
+    echo =====================================================================;
+    echo now filling no data vals ${outdir}/albers_${f##*/} with $extent $rows $cols $partitions;
+    mpirun python fill_large_raster.py ${outdir}/albers_${f##*/} $extent $rows $cols $partitions;    
+}
+
+export -f project
+export -f fill
+
+cat ${covar_file} | parallel project ${outdir}
+
+wait;
+
+# look at the special /dev/null assignment to mpirun input
+# from here: https://stackoverflow.com/a/43741729/3321542
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    fill $outdir $line > /dev/null < /dev/null
+done < ${covar_file}
