b06147dad2fd55bf507555171ccad77c73023877
Sudipta Basak <basaks@gmail.com>
2018-10-26 19:21:48 +1100

    Update dtype_based_albers.sh

--- dtype_based_albers.sh
+++ dtype_based_albers.sh
@@ -1,17 +1,8 @@
 export COMMON_OPTIONS="-co BLOCKXSIZE=256 -co BLOCKYSIZE=256 -co TILED=YES -co COMPRESS=LZW -co BIGTIFF=YES"
 # export GDAL_OPTS="-multi -co NUM_THREADS=ALL_CPUS -wo NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 50000"
-covar_file=/g/data/ge3/covariates/national/selected_covariates.txt
+covar_file=selected_covariates.txt
 
-outdir=/g/data/ge3/covariates/national_filled/albers_cubic_85_kernel3
-outdir2=/g/data/ge3/covariates/national_filled/albers_cubic_85_kernel3_final
-# number of pixels to interpolate to
-export extent=3
-export rows=10
-export cols=100
-export partitions=10
-
-# new no data
-export NODATA=-9999
+outdir=/path/to/outdir
 
 function project {
     outdir=$1
@@ -32,12 +23,3 @@ function project {
 }
 
 export -f project
-
-
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "while loop item  =================================";
-    # fill $outdir $line
-    ls $line;
-    fill ${outdir} ${line} > /dev/null < /dev/null;
-    echo converted $line;    
-done < ${covar_file}
