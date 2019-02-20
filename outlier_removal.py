70ca7f24ada36ec59bce0e8b4315b2eb11710592
Sudipta Basak <basaks@gmail.com>
2019-02-20 21:51:17 +1100

    added command line options via click

--- outlier_removal.py
+++ outlier_removal.py
@@ -1,15 +1,23 @@
+import click
 import geopandas as gpd
 from scipy import quantile
-input_shapefile = '/home/sudiptra/Documents/GA-cover2/geochem_sites.shp'
-output_shapefile = '/home/sudiptra/Documents/GA-cover2/geochem_sites_filtered' \
-                   '.shp'
-property = 'Zr_ppm_imp'
 
-quantiles = [1, 99]  # in percentage
 
-shp = gpd.read_file(input_shapefile)
-lq, uq = quantile(shp[property], [q/100 for q in quantiles])
+@click.command()
+@click.option('-i', '--input_shp', required=True, help='Input shapefile')
+@click.option('-o', '--output_shp', required=True, help='Output shapefile')
+@click.option('-p', '--property', required=True,
+              help='The property based on which quantiles will be calculated')
+@click.option('-q', '--quantiles', default=[1, 99], required=False,
+              help='Quantiles use for filtering')
+def remove_outliers(input_shp, output_shp, property, quantiles):
+    shp = gpd.read_file(input_shp)
+    lq, uq = quantile(shp[property], [q/100 for q in quantiles])
 
-shp_filetered = shp[(shp[property] > lq) & (shp[property] < uq)]
+    shp_filetered = shp[(shp[property] > lq) & (shp[property] < uq)]
 
-shp_filetered.to_file(output_shapefile)
+    shp_filetered.to_file(output_shp)
+
+
+if __name__ == '__main__':
+    remove_outliers()
