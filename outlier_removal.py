feae5962e4a6ecc5734fde6de50b07ef951825a1
Sudipta Basak <basaks@gmail.com>
2019-02-20 22:16:43 +1100

    minor changes

--- outlier_removal.py
+++ outlier_removal.py
@@ -8,11 +8,14 @@ from scipy import quantile
 @click.option('-o', '--output_shp', required=True, help='Output shapefile')
 @click.option('-p', '--property', required=True,
               help='The property based on which quantiles will be calculated')
-@click.option('-q', '--quantiles', default=[1, 99], required=False,
-              help='Quantiles use for filtering')
+@click.option('-q', '--quantiles', type=str, default="1, 99", required=False,
+              help='Quantiles use for filtering. Has to be comma separated '
+                   'list of two numbers in percentage, and not fraction. '
+                   'Example "2, 98".')
 def remove_outliers(input_shp, output_shp, property, quantiles):
     shp = gpd.read_file(input_shp)
-    lq, uq = quantile(shp[property], [q/100 for q in quantiles])
+    lq, uq = quantile(shp[property],
+                      [float(q)/100 for q in quantiles.split(',')])
 
     shp_filetered = shp[(shp[property] > lq) & (shp[property] < uq)]
 
