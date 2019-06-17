3a2953cbe4f2c06965712869f16917dcc365c778
Sudipta Basak <basaks@gmail.com>
2019-06-17 17:41:06 +1000

    add covariance/correlation heatmap/cluster

---
+++ covariance_analysis.py
@@ -0,0 +1,52 @@
+import rasterio as rio
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import scale
+import matplotlib.pylab as plt
+import seaborn as sns
+
+
+tifs = list(Path('/home/sudiptra/Documents/GA-cover2/').glob('*.tif'))
+
+# select 10
+selected = np.random.choice(tifs, 10)
+
+for s in selected:
+    with rio.open(s) as f:
+        if len(f.nodatavals) > 1:
+            raise AttributeError('file {s} has more than one band. We '
+                                 'currently support only single banded '
+                                 'rasters.'.format(s=s))
+
+data = {Path(s).stem: rio.open(s).read(masked=True).flatten()
+        for s in selected}
+
+mask = pd.DataFrame.from_dict(data={k: v.mask for k, v in data.items()})
+df = pd.DataFrame.from_dict(data={k: v.data for k, v in data.items()})
+print('Original data with shape {}'.format(df.shape))
+df = df[~mask.any(axis=1)]
+
+print('After removing masked values: shape {}'.format(df.shape))
+
+# sample now
+dfs = df.sample(frac=0.01)
+cols = dfs.columns
+
+# compute covariance matrix
+df_data = scale(dfs)
+cov_mat = np.cov(df_data, rowvar=False)
+
+f, ax = plt.subplots(figsize =(9, 8))
+corrmat = pd.DataFrame(df_data, columns=cols).corr()
+sns.heatmap(corrmat, ax=ax, cmap="YlGnBu", linewidths=0.1)
+plt.savefig('{}.png'.format('results/Correlation-Matrix'))
+
+f, ax = plt.subplots(figsize=(9, 8))
+sns.heatmap(cov_mat, ax=ax, cmap="YlGnBu", linewidths=0.1)
+plt.savefig('{}.png'.format('results/Covariance-Matrix'))
+
+f, ax = plt.subplots(figsize=(9, 8))
+cg = sns.clustermap(corrmat, cmap="YlGnBu", linewidths=0.1)
+plt.setp(cg.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
+plt.savefig('{}.png'.format('results/Correlation-Clustermap'))
