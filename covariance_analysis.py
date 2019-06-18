4bb1876a6c90eb8ec984c4eeb6a3566b30bbc429
Sudipta Basak <basaks@gmail.com>
2019-06-18 19:59:42 +1000

    problem with joblib in nci...go back to list comprehension

--- covariance_analysis.py
+++ covariance_analysis.py
@@ -1,32 +1,41 @@
-from joblib import Parallel, delayed
-import rasterio as rio
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import rasterio as rio
 from sklearn.preprocessing import scale
 import matplotlib.pylab as plt
 import seaborn as sns
 
 
-tifs = list(Path('/home/sudiptra/Documents/GA-cover2/').glob('*.tif'))
+tifs = list(Path('/home/sudiptra/Documents/GA-cover2/').glob('*.tif'))[:5]
 
-# select 10
-selected = np.random.choice(tifs, 10)
-
-for s in selected:
+for s in tifs:
     with rio.open(s) as f:
         if len(f.nodatavals) > 1:
             raise AttributeError('file {s} has more than one band. We '
                                  'currently support only single banded '
                                  'rasters.'.format(s=s))
+total_length = None
 
 
 def _read_file(s):
+    global total_length
+    print('Reading file {}'.format(s))
+    f = rio.open(s)
+    if total_length is None:
+        total_length = f.height * f.width
+        print('File {} with size {}'.format(s, total_length))
+    else:
+        print('File {} with size {}'.format(s, f.height*f.width))
+        if total_length != f.height * f.width:
+            raise AttributeError("file {} is not equal to the others".format(s))
+
     return rio.open(s).read(masked=True).flatten()
 
 
-p_data = Parallel(n_jobs=-1)(delayed(_read_file)(s) for s in selected)
-data = {Path(s).stem: d for s, d in zip(selected, p_data)}
+p_data = [_read_file(s) for s in tifs]
+
+data = {Path(s).stem: d for s, d in zip(tifs, p_data)}
 
 mask = pd.DataFrame.from_dict(data={k: v.mask for k, v in data.items()})
 df = pd.DataFrame.from_dict(data={k: v.data for k, v in data.items()})
@@ -43,7 +52,7 @@ cols = dfs.columns
 df_data = scale(dfs)
 cov_mat = np.cov(df_data, rowvar=False)
 
-f, ax = plt.subplots(figsize =(9, 8))
+f, ax = plt.subplots(figsize=(9, 8))
 corrmat = pd.DataFrame(df_data, columns=cols).corr()
 sns.heatmap(corrmat, ax=ax, cmap="YlGnBu", linewidths=0.1)
 plt.savefig('{}.png'.format('results/Correlation-Matrix'))
