1390704d564d1398948b2aac0cb320a81ed35108
Sudipta Basak <basaks@gmail.com>
2019-07-06 07:33:54 +1000

    cluster/classification feature analysis and vis

---
+++ cluster_analysis.py
@@ -0,0 +1,76 @@
+"""
+This script takes a classification/cluster output (raster), and also the
+features of the cluster in raster form to produce cluster interpretation plots.
+"""
+
+import click
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import rasterio as rio
+import matplotlib.pylab as plt
+import seaborn as sns
+
+
+def __check_raster(tifs):
+    for s in tifs:
+        with rio.open(s) as f:
+            if len(f.nodatavals) > 1:
+                raise AttributeError('file {s} has more than one band. We '
+                                     'currently support only single banded '
+                                     'rasters.'.format(s=s))
+
+
+total_length = None
+
+
+def _read_file(s):
+    global total_length
+    print('Reading file {}'.format(s))
+    f = rio.open(s)
+    if total_length is None:
+        total_length = f.height * f.width
+        print('File {} with size {}'.format(s, total_length))
+    else:
+        print('File {} with size {}'.format(s, f.height*f.width))
+        if total_length != f.height * f.width:
+            raise AttributeError("file {} is not equal to the others".format(s))
+
+    return rio.open(s).read(masked=True).flatten()
+
+
+@click.command()
+@click.option('-r', '--raster', required=True, help='Input raster')
+@click.option('-f', '--features_dir', required=True,
+              help='Dir with features that contributed to this '
+                   'classification/clustering output')
+def remove_outliers(raster, features_dir):
+
+    tifs = list(Path(features_dir).glob('*.tif'))
+
+    p_data = [_read_file(s) for s in tifs]
+
+    data = {Path(s).stem: d for s, d in zip(tifs, p_data)}
+
+    mask = pd.DataFrame.from_dict(data={k: v.mask for k, v in data.items()})
+    df = pd.DataFrame.from_dict(data={k: v.data for k, v in data.items()})
+    print('Original data with shape {}'.format(df.shape))
+    df = df[~mask.any(axis=1)]
+
+    print('After removing masked values: shape {}'.format(df.shape))
+
+    classes = _read_file(raster)
+
+    # sample now
+    cols = df.columns
+    df['class'] = classes
+    dfs = df.sample(frac=0.1)
+
+    for c in cols:
+        plt.figure()
+        sns.boxplot(x='class', y=c, data=dfs)
+        plt.savefig(c + '.png')
+
+
+if __name__ == '__main__':
+    remove_outliers()
