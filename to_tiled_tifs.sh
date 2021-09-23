3475fe0af720a1eeae476d81c34b33c8484f468d
Sudipta Basak <2975463+basaks@users.noreply.github.com>
2021-09-24 07:58:08 +1000

    Update to_tiled_tifs.sh

--- to_tiled_tifs.sh
+++ to_tiled_tifs.sh
@@ -2,3 +2,6 @@ gdal_translate -of GTiff -co "BLOCKXSIZE=128" -co "BLOCKYSIZE=128" -co "TILED=YE
 
 # sampling and compressing
 gdalwarp -r near -tr 1000 1000 -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" -co "TILED=YES" -wo NUM_THREADS=ALL_CPUS -wo SOURCE_EXTRA=1000 -co COMPRESS=LZW dem_fill_aspect.tif dem_fill_aspect_resampled.tif -overwrite
+
+# sampling and compressing with 16 bit interger output
+gdalwarp -r near -tr 1000 1000 -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" -co "TILED=YES" -wo NUM_THREADS=ALL_CPUS -wo SOURCE_EXTRA=100 -co COMPRESS=LZW dem_fill_aspect.tif dem_fill_aspect_resampled.tif -ot Int16 -overwrite
