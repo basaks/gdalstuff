edd9949dffcd77ae72987fb223f66308503c19b9
Sudipta Basak <2975463+basaks@users.noreply.github.com>
2021-09-24 07:50:17 +1000

    Update to_tiled_tifs.sh

--- to_tiled_tifs.sh
+++ to_tiled_tifs.sh
@@ -1,1 +1,4 @@
 gdal_translate -of GTiff -co "BLOCKXSIZE=128" -co "BLOCKYSIZE=128" -co "TILED=YES" -co compress=LZW dem_fill.tif dem_fill_tiled.tif
+
+# sampling and compressing
+gdalwarp -r near -tr 1000 1000 -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" -co "TILED=YES" -wo NUM_THREADS=ALL_CPUS -wo SOURCE_EXTRA=1000 -co COMPRESS=LZW dem_fill_aspect.tif dem_fill_aspect_resampled.tif -overwrite
