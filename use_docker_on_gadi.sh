3221474ad134bd2b7b1a5f2e7edc415cbcbaf88d
Sudipta Basak <basaks54321@users.noreply.github.com>
2022-04-21 19:45:15 +1000

    convert no data to nan in addition

--- use_docker_on_gadi.sh
+++ use_docker_on_gadi.sh
@@ -1,3 +1,8 @@
 module load singularity
 singularity pull --dir . docker://osgeo/gdal
-singularity exec --bind /g/data/ge3/covariates/national_albers_filled_new/albers_cropped/:/albers_cropped/ --bind /g/data/ge3/sudipta/jobs/cogs/:/cogs/ gdal_latest.sif gdalwarp /albers_cropped/dem_fill.tif /cogs/dem_fill.tif -of COG -co BIGTIFF=YES -co COMPRESS=LZW
+singularity exec \
+  --bind /g/data/ge3/covariates/national_albers_filled_new/albers_cropped/:/albers_cropped/ \
+  --bind /g/data/ge3/sudipta/jobs/cogs/:/cogs/ \
+  /g/data/ge3/sudipta/jobs/docker/gdal_latest.sif \
+  gdalwarp -srcnodata -9999 -dstnodata 0 \
+  /albers_cropped/dem_fill.tif /cogs/dem_fill.tif -of COG -co BIGTIFF=YES -co COMPRESS=LZW
