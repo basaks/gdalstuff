697799bd82738d1dbef6e3dd5261a6e7bee8ec2b
Sudipta Basak <basaks54321@users.noreply.github.com>
2022-05-03 19:44:32 +1000

    a dockerised parallel function

--- use_docker_on_gadi.sh
+++ use_docker_on_gadi.sh
@@ -35,6 +35,39 @@ singularity exec \
   /cogs/demh1sv1_0.tif /cogs/demh1sv1_0_80m_albers.tif -of COG -co BIGTIFF=YES -co COMPRESS=LZW
 
 
+
+function dockerised_crop {
+    f=$1
+    singularity exec \
+      --bind /g/data/ge3/sudipta/jobs/cogs/:/cogs/ \
+      --bind /g/data/ge3/covariates/national_albers_filled_new/albers_cropped/:/albers_cropped/ \
+      /g/data/ge3/sudipta/jobs/docker/gdal_latest.sif \
+      gdalwarp \
+      -dstnodata 'nan' \
+      -t_srs EPSG:3577 \
+      -tr 80.0 80.0 \
+      -te  -2000000.000 -4899990.000 2200020.000 -1050000.000 \
+      /albers_cropped/${f##*/} /cogs/majors_covs_80m/${f##*/} -of COG -co BIGTIFF=YES -co COMPRESS=LZW;
+}
+
+export -f dockerised_crop
+cat /path/to/some/text/file_with_raster_paths.txt | parallel dockerised_crop
+
+for f in `cat /g/data/ge3/john/MAJORS/CNN_test/2022_list.txt`;
+  do singularity exec \
+  --bind /g/data/ge3/sudipta/jobs/cogs/:/cogs/ \
+  --bind /g/data/ge3/covariates/national_albers_filled_new/albers_cropped/:/albers_cropped/ \
+  /g/data/ge3/sudipta/jobs/docker/gdal_latest.sif \
+  gdalwarp \
+  -dstnodata 'nan' \
+  -t_srs EPSG:3577 \
+  -tr 80.0 80.0 \
+  -te  -2000000.000 -4899990.000 2200020.000 -1050000.000 \
+  /albers_cropped/${f##*/} /cogs/majors_covs_80m/${f##*/} -of COG -co BIGTIFF=YES -co COMPRESS=LZW;
+done
+
+
+
 #Upper Left  (-2000000.000,-1050000.000) (114d22'17.56"E,  8d43'40.51"S)
 #Lower Left  (-2000000.000,-4899990.000) (108d 4'11.04"E, 43d 6'57.19"S)
 #Upper Right ( 2200020.000,-1050000.000) (151d21'56.84"E,  8d27'28.29"S)
