0f74e52e304e79fc1ed8650d984c29d373b9b132
Sudipta Basak <basaks@gmail.com>
2022-07-20 17:42:27 +1000

    add wa and easter corridor crops

--- use_docker_on_gadi.sh
+++ use_docker_on_gadi.sh
@@ -278,6 +278,29 @@ function dockerised_conversion_from_large_no_data {
 }
 
 
+#
+#[15/07 15:17] John Wilford
+#built 2 new 30 geology class covariates  - /g/data/ge3/data/data_in_transit/Lithology_Surface_Geology30M.tif
+# like 1
+#
+#[15/07 15:17] John Wilford
+#/g/data/ge3/data/data_in_transit/Lithology_Surface_Geology_Major30M.tif
+# like 1
+#
+#[15/07 15:17] John Wilford
+#these won't have the correct number of pixel and will need to be clipped etc...
+#
+#[15/07 15:20] John Wilford
+#nodata for Lithology_Surface_Geology30M.tif == 255 and Lithology_Surface_Geology_Major30M.tif == -128
+#
+#[15/07 15:21] John Wilford
+#sorry about that I should have made them the same - how are we define the 8 bit datasets in terms of nodata value ?
+#
+#[15/07 16:39] John Wilford
+#lots to get through ...let me know if you plan to work over the weekend...Im heading off now
+
+
+
 function dockerised_crop_rad2016k_th_fixed {
     f=$1
     singularity exec \
@@ -294,6 +317,15 @@ function dockerised_crop_rad2016k_th_fixed {
       /albers_cropped/${f##*/} /cogs/dale_fixed/${f##*/} -of COG -co BIGTIFF=YES -co COMPRESS=LZW;
     echo ====== processed ${f} ====== ;
 }
+
+# lots of iterations of of wa conductivity models using both NN and also traditional ml models
+# some minor enhancements of landshark code + massively parallel prediction
+# lots of cenozoic depth model predictions using aem-depth package and some minor enhancements on that package
+# we are starting to write this cenozoic depth modelling paper
+# started looking at 2 and half d modelling approach for cenozoic basin depth by combining both the 2d depth model
+# (predictions) + Seb cenozoic depth interpretations + the drill holes
+# continuing the 80m national covariates work - almost settled now
+
 
 function dockerised_crop_water_fixed {
     f=$1
@@ -372,3 +404,23 @@ done
 #Upper Right ( 2468112.380,-1048240.533) (153d40'49.48"E,  8d 2'22.61"S)
 #Lower Right ( 2468112.380,-4964360.533) (161d31'39.81"E, 42d47'51.34"S)
 #Center      (  166907.380,-3006300.533) (133d42'47.48"E, 27d41'54.64"S)
+
+function ec_crop {
+  f=$1
+  echo  will convert ${f} into ec_crop/${f##*/}
+  gdalwarp -te 627320 -3763932 774876 -3674850 ${f} ec_crop/${f##*/} -overwrite
+}
+
+function wa_crop {
+  f=$1
+  echo  will convert ${f} into wa_covariates/${f##*/}
+  gdalwarp -te -1968200 -4004450 -215350 -1235420 ${f} wa_covariates/${f##*/} -overwrite
+}
+# -te -1968200 -4004450 -215350 -1235420
+
+# WA files
+#Upper Left  (-1668147.000,-3268289.000) (114d36'59.34"E, 29d 1'50.40"S)
+#Lower Left  (-1668147.000,-3933119.000) (113d37'35.72"E, 34d55' 3.57"S)
+#Upper Right ( -783670.000,-3268289.000) (123d47'38.46"E, 29d49'11.09"S)
+#Lower Right ( -783670.000,-3933119.000) (123d19'18.55"E, 35d45'32.86"S)
+#Center      (-1225908.500,-3600704.000) (118d49'54.63"E, 32d26'53.77"S)
