d6dd4430e98867960eff8f90dfeb8714418eafbd
Sudipta Basak <basaks@gmail.com>
2023-10-12 09:27:04 +1100

    more cut example via for loop

--- delete_jobs_nci.sh
+++ delete_jobs_nci.sh
@@ -1,1 +1,3 @@
 parallel -u qdel {} ::: `nqstat -P ge3 | grep 648 | cut -b 1-8`
+
+for f in `ls *.tif -1`; do echo $f; nf=`echo $f | cut -b 49-`; echo moving $f to cnn_hw4_7_classes_more_covs_$nf; mv $f cnn_hw4_7_classes_more_covs_$nf; done
